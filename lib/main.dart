3f764b907d9ab2f8d7ec12c8b99a90a3de0848c6
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-04 11:03:52 +0200

    [Screen] Create main & homescreen ok

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() => runApp(NearMeApp());
+
+class NearMeApp extends StatelessWidget {
+  const NearMeApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Near Me',
+      home: HomeScreen(),
+      theme:
+          ThemeData(scaffoldBackgroundColor: Color.fromRGBO(38, 23, 23, 1.0)),
+    );
+  }
+}
