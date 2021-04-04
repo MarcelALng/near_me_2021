022908ef9a3fce092aa3a78f5f97debeead3330b
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-04 21:29:00 +0200

    [Screen] add logo and google font to home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      bottomNavigationBar: BottomAppBar(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/logofork.png'),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text(
+              'Near Me',
+              style: GoogleFonts.quicksand(
+                  color: Colors.white38,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
