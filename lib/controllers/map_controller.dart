6013e35c53a921d524bfe3e83c2b6ac766d27f23
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-08 02:27:20 +0200

    [Constant] Refactor from map controller to global constant OK

--- lib/controllers/map_controller.dart
+++ lib/controllers/map_controller.dart
@@ -1,20 +1,30 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:near_me_2021/models/place_model.dart';
+import '../models/place_model.dart';
+
+import '../constants/global_constant.dart';
 
 class MapController {
   Future<dynamic> getPlaces() async {
     List<PlaceModel> _places = [];
-    final url =
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyAxKT7TkuoQ5xp20RPeShcZI5bfs1nRjFc";
+
+    Map<String, String> _parameters = {
+      'location': '-33.8670522,151.1957362',
+      'radius': '500',
+      'types': 'food',
+      'key': GlobalConstant.apiKey
+    };
+    // "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=apiKey";
+    final _url =
+        Uri.https(GlobalConstant.authority, GlobalConstant.path, _parameters);
+    print(_url.toString());
     // need to enable PLACES API( Android key here) here https://console.cloud.google.com/marketplace/product/google/places-backend.googleapis.com?project=nearme-309821
-    Uri.https(authority, unencodedPath);
-    http.Response _request = await http.get(url);
+    http.Response _request = await http.get(_url);
     final List<dynamic> _jsonData = json.decode(_request.body)["results"];
     print(_jsonData);
     _jsonData.forEach((place) => _places.add(PlaceModel.fromJSON(place)));
     print(_places[0].price);
-    // print(_request.body);
+    print(_request.body);
   }
 }
